c92431d0892c95984a945b523a39f8208d0824de
Lucas Mongrain <xeaprog@gmail.com>
2019-10-19 13:00:32 -0400

    add read and write function in the header

--- RS485/RS485.h
+++ RS485/RS485.h
@@ -15,6 +15,8 @@ namespace RS485
     #define WRITER_QUEUE_SIZE 5
 
     void init(uint8_t adress, uint32_t prefered_sleep_time = 20);
+    uint8_t read(uint8_t* cmd_array, uint8_t nb_command, uint8_t* data_buffer);
+    void write(uint8_t slave, uint8_t cmd, uint8_t nb_byte, uint8_t* data_buffer);
 
     typedef struct RS485_reader_message_struct
     {
