51fb44bc26cbd884bf6935ea8b6eb5c689435179
Lucas Mongrain <xeaprog@gmail.com>
2021-08-07 22:29:02 -0400

    new define for RS485

--- RS485/RS485_definition.h
+++ RS485/RS485_definition.h
@@ -2,9 +2,13 @@
 //              SLAVE DEFINITION
 //###################################################
 
-#define SLAVE_BACKPLANE 0
-#define SLAVE_IO 1
-#define SLAVE_KILLMISSION 2
+#define SLAVE_PSU0 0
+#define SLAVE_PSU1 1
+#define SLAVE_PSU2 2
+#define SLAVE_PSU3 3
+#define SLAVE_KILLMISSION 4
+#define SLAVE_ESC 5
+#define SLAVE_IO_ 6
 #define SLAVE_STATE_SCREEN 7
 
 //###################################################
@@ -15,10 +19,11 @@
 #define CMD_MISSION 0
 #define CMD_KILL 1
 
-// define backplane
-
+// define backplane/PSU
 #define CMD_VOLTAGE 0
 #define CMD_CURRENT 1
+
+// define backplane/ESC
 #define CMD_READ_MOTOR 2
 #define CMD_ACT_MOTOR 3
 #define CMD_PWM 4
@@ -28,6 +33,7 @@
 #define CMD_IO_DROPPER_ACTION  1
 #define CMD_IO_TORPEDO_ACTION  2
 #define CMD_IO_ARM_ACTION 3
+#define CMD_IO_LEAK_SENSOR 4
 
 //###################################################
 //              DATA DEFINITION
@@ -41,3 +47,6 @@
 
 #define DATA_IO_ARM_CLOSE 0
 #define DATA_IO_ARM_OPEN 1
+
+#define DATA_IO_LEAK_SENSOR_DRY 0
+#define DATA_IO_LEAK_SENSOR_LEAK 1
