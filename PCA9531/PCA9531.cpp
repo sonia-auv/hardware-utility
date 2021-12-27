8c538a811bf374d53b4b538d60244e64d7711198
Francis Alonzo <44241055+supertoto29@users.noreply.github.com>
2021-12-27 18:10:59 -0500

    change PCA9531 object

--- PCA9531/PCA9531.cpp
+++ PCA9531/PCA9531.cpp
@@ -17,8 +17,18 @@ PCA9531::~PCA9531()
 
 void PCA9531::setLEDs(uint16_t state)
 {
-    setSelectorLEDs(state, LS0);
-    setSelectorLEDs((state >> 8), LS1);
+    setSelectorLEDs(state, LS0_REG);
+    setSelectorLEDs((state >> 8), LS1_REG);
+}
+
+void PCA9531::setSelectorLEDs(uint8_t state, uint8_t selector)
+{
+    char temp[2];
+
+    temp[0] = selector;
+    temp[1] = state;
+
+    _i2c->write(addr, temp, 2);
 }
 
 void PCA9531::setPrescaler(uint8_t prescaler, uint8_t pwm_register)
@@ -27,11 +37,11 @@ void PCA9531::setPrescaler(uint8_t prescaler, uint8_t pwm_register)
 
     if(pwm_register == 1)
     {
-        temp[0] = PSC1;
+        temp[0] = PSC1_REG;
     }
     else
     {
-        temp[0] = PSC0;
+        temp[0] = PSC0_REG;
     }
 
     temp[1] = prescaler;
@@ -45,24 +55,14 @@ void PCA9531::setDutyCycle(uint8_t duty_cycle, uint8_t pwm_register)
 
     if(pwm_register == 1)
     {
-        temp[0] = PWM1;
+        temp[0] = PWM1_REG;
     }
     else
     {
-        temp[0] = PWM0;
+        temp[0] = PWM0_REG;
     }
 
     temp[1] = duty_cycle;
 
     _i2c->write(addr, temp, 2);    
-}
-
-void PCA9531::setSelectorLEDs(uint8_t state, uint8_t selector)
-{
-    char temp[2];
-
-    temp[0] = selector;
-    temp[1] = state;
-
-    _i2c->write(addr, temp, 2);
 }
