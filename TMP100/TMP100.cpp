644a9aff9f6e4c4abf108e6b3be7f5aa712c33f3
Olivier <olivierjuteaud@gmail.com>
2022-04-29 22:22:41 -0400

    addition of configuration of TMP100

--- TMP100/TMP100.cpp
+++ TMP100/TMP100.cpp
@@ -11,10 +11,18 @@ TMP100::TMP100(I2C *i2c, char address)
     _addr = address;
 }
 
+TMP100::TMP100(I2C *i2c, char address, TMP100_resolution_t resolution,TMP100_ShutdownMode_t shutDownMode)
+{
+    _i2c = i2c;
+    _addr = address;
+
+    setResolution(resolution);
+    setShutdownMode(shutDownMode);
+}
+
 TMP100::~TMP100()
 {
 }
-
 
 float TMP100::getTemperature(void)
 {
@@ -25,6 +33,62 @@ float TMP100::getTemperature(void)
     }
 
     return TempRegToFloat(val_reg);
+}
+
+void TMP100::setResolution(TMP100_resolution_t resolution)
+{
+    uint8_t currentConfig;
+    bool unexpextedValue = false;
+    readTMP100(REG_CONFIG, &currentConfig);
+    currentConfig &= ~MASK_CONFIG_RES; //remove current resolution config
+
+    switch (resolution)
+    {
+    case 9_BITS:
+        currentConfig |= 0b00;
+        break;
+    case 10_BITS:
+        currentConfig |= 0b01;
+        break;    
+    case 11_BITS:
+        currentConfig |= 0b10;
+        break;
+    case 12_BITS:
+        currentConfig |= 0b11;
+        break;   
+    default:
+        unexpextedValue = true;
+        break;
+    }
+
+    if(!unexpextedValue){
+        writeTMP100(REG_CONFIG, &currentConfig);
+    }
+}
+
+void TMP100::setShutdownMode(TMP100_ShutdownMode_t mode)
+{
+    uint8_t currentConfig;
+    bool unexpextedValue = false;
+    readTMP100(REG_CONFIG, &currentConfig);
+    currentConfig &= ~MASK_CONFIG_SD; //remove current shotdown config
+
+    switch (mode)
+    {
+    case CONTINUOUS:
+        currentConfig |= 0;
+        break;
+    case SINGLE:
+        currentConfig |= 1;
+        break;
+    default:
+        unexpextedValue = true;
+        break;
+    }
+
+    if(!unexpextedValue){
+        writeTMP100(REG_CONFIG, &currentConfig);
+    }
 }
 
 TMP100_error_code_t TMP100::readTMP100(char cmd, uint16_t *value)
@@ -42,6 +106,41 @@ TMP100_error_code_t TMP100::readTMP100(char cmd, uint16_t *value)
 
     _i2c->read(_addr+1,buff,2);
     *value = (buff[0] << 8) | buff[1]; 
+}
+
+TMP100_error_code_t TMP100::readTMP100(char cmd, uint8_t *value)
+{
+    char buff;
+    int16_t status;
+
+    if(value == NULL){
+        return ERR_NULL_PTR;
+    }
+
+    if(_i2c->write(_addr,&cmd,1,true)){
+        return ERR_INVALID_READ;
+    }
+
+    _i2c->read(_addr+1,&buff,1);
+    *value = buff; 
+}
+
+
+void TMP100::writeTMP100(char cmd, uint16_t reg)
+{
+    char buffer[3];
+    buffer[0] = cmd;
+    buffer[1] = (char) ((reg & 0xFF00) >> 8);
+    buffer[2] = (char) (reg & 0x00FF);
+    _i2c->write(_addr,buffer,3);
+}
+
+void TMP100::writeTMP100(char cmd, uint8_t reg)
+{
+    char buffer[2];
+    buffer[0] = cmd;
+    buffer[1] = (char) (reg);
+    _i2c->write(_addr,buffer,2);
 }
 
 float TMP100::TempRegToFloat(uint16_t reg)
--- TMP100/TMP100.h
+++ TMP100/TMP100.h
@@ -15,6 +15,19 @@ typedef enum TMP100_error_code{
     ERR_INVALID_READ    =-2.
 } TMP100_error_code_t;
 
+typedef TMP100_resolution{
+    CONTINUOUS,
+    SINGLE
+}TMP100_resolution_t;
+
+typedef TMP100_ShutdownMode{
+    9_BITS,
+    10_BITS,
+    11_BITS,
+    12_BITS
+}TMP100_ShutdownMode_t
+
+
 
 /**
  * @brief TMP100 I2C class
@@ -32,6 +45,9 @@ public:
  */
 
     TMP100(I2C *i2c, char address);
+
+    TMP100(I2C *i2c, char address, TMP100_resolution_t resolution,TMP100_ShutdownMode_t shutDownMode)
+
     
 /**
  * @brief Destroy the TMP100 object
@@ -45,6 +61,10 @@ public:
  * @return float temperature (in celsius)
  */
     float getTemperature(void);
+
+    void setShutdownMode(TMP100_ShutdownMode_t mode);
+    void setResolution(TMP100_resolution_t resolution);
+
 
 private:
     char _addr;
@@ -75,7 +95,19 @@ private:
      * @param cmd 
      * @param value 
      */
-    void TMP100::readTMP100(char cmd, uint16_t *value);
+    TMP100_error_code_t readTMP100(char cmd, uint16_t *value);
+
+    TMP100_error_code_t readTMP100(char cmd, uint8_t *value);
+
+
+    /**
+     * @brief Read a 16 bit value (2 consecutive register)
+     * 
+     * @param cmd 
+     * @param value 
+     */
+    void writeTMP100(char cmd, uint16_t reg)
+    void writeTMP100(char cmd, uint8_t reg)
 
 
     //register definition
@@ -84,6 +116,8 @@ private:
     const uint8_t REG_T_LOW     = 0x02;
     const uint8_t REG_T_HICH    = 0x02;
 
+    const uint8_t MASK_CONFIG_RES    = 0b01100000;
+    const uint8_t MASK_CONFIG_SD     = 0b00000001;
 
 
 
