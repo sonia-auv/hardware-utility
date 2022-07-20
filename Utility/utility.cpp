ceb04d48c5e37c98e248457e8390ed5bc7b42e6f
0x72d0 <xeaprog@gmail.com>
2022-07-20 11:56:15 -0400

    i'm stupid

--- Utility/utility.cpp
+++ Utility/utility.cpp
@@ -60,7 +60,7 @@ double_t readfromAnalog(AnalogIn input, double_t voltageRef, double_t R1, double
     return voltage_battery / (double_t)i;
 }
 
-void isAliveThread(RS485* rs)
+void isAliveThread(RS485* rs, bool optional_watchdog = false)
 {
     uint8_t cmd_array[1]={CMD_IS_ALIVE};
     uint8_t buffer[255]={0};
@@ -68,19 +68,10 @@ void isAliveThread(RS485* rs)
     while(true)
     {
         rs->read(cmd_array, 1, buffer);
-        rs->write(rs->getBoardAdress(), CMD_IS_ALIVE, 0, buffer);
-    }
-}
-
-void isAliveThreadWithWatchdog(RS485* rs)
-{
-    uint8_t cmd_array[1]={CMD_IS_ALIVE};
-    uint8_t buffer[255]={0};
-
-    while(true)
-    {
-        rs->read(cmd_array, 1, buffer);
-        Watchdog::get_instance().kick();
+        if(optional_watchdog)
+        {
+            Watchdog::get_instance().kick();
+        }
         rs->write(rs->getBoardAdress(), CMD_IS_ALIVE, 0, buffer);
     }
 }
